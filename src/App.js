fddf5bf541f013117e00099e0aa1a36aee77d6a1
nana <comfortappiah.ca@gmail.com>
2022-08-20 15:12:20 +0000

    redux setup

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import AddUserForm from "./components/AddUserForm";
 import AllUsers from "./components/AllUsers";
 
 function App() {
-	const [users, setUsers] = useState([
-		{ name: "John", email: "jon@gmail.com", gen: "4", id: "edjjdjdjdjdkskw" },
-		{ name: "Ama", email: "ama@gmail.com", gen: "48", id: "udiue9893e03-kked" },
-		{ name: "tony", email: "gmail.com", gen: "40", id: "uuueiriedm003wwsw" },
-		{
-			name: "kachi ",
-			email: "kachigmail.com",
-			gen: "90",
-			id: "090390kiocjifiorhi4",
-		},
-	]);
+	const [users, setUsers] = useState(
+		[
+		// { name: "John", email: "jon@gmail.com", gen: "4", id: "edjjdjdjdjdkskw" },
+		// { name: "Ama", email: "ama@gmail.com", gen: "48", id: "udiue9893e03-kked" },
+		// { name: "tony", email: "gmail.com", gen: "40", id: "uuueiriedm003wwsw" },
+		// {
+		// 	name: "kachi ",
+		// 	email: "kachigmail.com",
+		// 	gen: "90",
+		// 	id: "090390kiocjifiorhi4",
+		// },
+	]
+	);
 	//add new user to the users array(state)
 	const AddNewUser = (user) => {
 		user.id = Math.random().toString(36);
@@ -28,7 +30,9 @@ function App() {
 	//delete user from users array
 	const deleteUser = (id) => {
 		//tenary operator to delete user from users array
-		setUsers(users.filter((user) => user.id !== id));
+		// setUsers(users.filter((user) => user.id !== id));
+
+		setUsers(users.filter((user) => (user.id !== id ? user : null)));
 
 		//normal if else statement to delete user from users array
 		// setUsers(
@@ -51,8 +55,17 @@ function App() {
 		// 	})
 		// );
 		//tenary operator to edit user from users array
-		setUsers(users.map((user) => (user.id === id ? newInfo : user)));
+		// setUsers(users.map((user) => (user.id === id ? newInfo : user)));
+		setUsers(
+			users.map((user) => {
+				if (user.id === id) {
+					return newInfo;
+				}
+				return user;
+			})
+		);
 	};
+
 	return (
 		<>
 			<Container>
