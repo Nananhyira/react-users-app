986dd9ab63e3cd967fcc694391c2b016639777eb
nana <comfortappiah.ca@gmail.com>
2022-09-07 19:04:23 +0000

    comments

---
+++ src/store/Store.js
@@ -0,0 +1,7 @@
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import UsersReducer from "../reducer/UsersReducer";
+import thunk from "redux-thunk";
+
+let Store = createStore(UsersReducer, applyMiddleware(thunk));
+
+export default Store;
